27cb6d7b054dda59b2a7615039ec02a859b6bb74
ADMIN <ADMIN@DESKTOP-KSF6HMH>
2018-05-25 11:32:54 +0530

    This is Employee module

---
+++ src/com/app/Employee.java
@@ -0,0 +1,23 @@
+package com.app;
+
+public class Employee {
+int a;
+String s;
+public int getA() {
+	return a;
+}
+public void setA(int a) {
+	this.a = a;
+}
+public String getS() {
+	return s;
+}
+public void setS(String s) {
+	this.s = s;
+}
+@Override
+public String toString() {
+	return "Employee [a=" + a + ", s=" + s + "]";
+}
+
+}
